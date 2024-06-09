d6e88e59d2cfaf3f7d4b0375160e7b99f68eecb4
Pompomtomm <tomlebreton0@gmail.com>
2024-06-09 12:29:16 +0200

    début du html css

---
+++ header.php
@@ -0,0 +1,14 @@
+<?php 
+include_once('head.php');
+?>
+
+<body>
+    <header>
+        <img class="logo" src="03.jpg" alt="logo confiz">
+        <nav id="navigation">
+            <p>Accueil</p>
+            <p>Boutiques</p>
+            <p>Connexion</p>
+        <nav>
+    </header>
+</body>

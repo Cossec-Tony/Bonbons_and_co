1d4211b20ac3144dfab3f3d962aeff3d4af10ab4
hugi <hugo.cossec@gmail.com>
2024-06-11 09:01:14 +0200

    init bdd

--- head.php
+++ head.php
@@ -6,8 +6,5 @@
     <title>Confiz</title>
     <link rel="shortcut icon" type="image/x-icon" href="03-Photoroom.png" />
     <link rel="stylesheet" href="style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Itim&display=swap" rel="stylesheet">
 </head>
 </html>
---
+++ testbdd.php
@@ -0,0 +1,8 @@
+<?php
+include_once("db.php");
+
+$a = requete("SELECT * FROM utilisateurs");
+
+echo ("<pre>");
+print_r($a);
+echo ("</pre>");

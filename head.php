45ea23da042ced337f65f6b85314dd9b494fa0f9
Pompomtomm <tomlebreton0@gmail.com>
2024-06-11 09:17:00 +0200

    Update head.php

--- head.php
+++ head.php
@@ -6,5 +6,8 @@
     <title>Confiz</title>
     <link rel="shortcut icon" type="image/x-icon" href="img/03-Photoroom.png" />
     <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Itim&display=swap" rel="stylesheet">
 </head>
 </html>

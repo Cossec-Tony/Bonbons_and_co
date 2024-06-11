e66883f55a5da6fd50bd56016a272a129829a2a5
Pompomtomm <tomlebreton0@gmail.com>
2024-06-11 15:47:57 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -8,24 +8,12 @@ include_once('head.php');
     <main>
         <section class="form-container">
             <h2>Inscription Utilisateur</h2>
-            <form action="submit.php" method="post">
-                <label for="nom">Nom:</label>
-                <input type="text" id="nom" name="nom" required>
-
-                <label for="prenom">Prénom:</label>
-                <input type="text" id="prenom" name="prenom" required>
-
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email" required>    
-                
+            <form action="submit.php" method="post">    
                 <label for="username">Nom d'utilisateur:</label>
-                <input type="text" id="username" name="username" required>
+                <input type="text" id="username" name="username" placeholder="Username" required>
                 
                 <label for="password">Mot de passe:</label>
-                <input type="password" id="password" name="password" required>
-                
-                <label for="ddn">Date de naissance:</label>
-                <input type="date" id="ddn" name="ddn" required>
+                <input type="password" id="password" name="password" placeholder="Password" required>
                 
                 <input type="submit" value="Envoyer">
             </form>

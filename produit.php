76253240ca408f84248c04db34bd0ae13d6b5dcc
Pompomtomm <tomlebreton0@gmail.com>
2024-06-11 09:46:01 +0200

    update produits

--- produit.php
+++ produit.php
@@ -3,7 +3,13 @@ include_once('head.php');
 include_once('header.php');
 ?>
 <body>
-    <container>
-        <img id="bonbon" src='img/bonbon1.jpg' alt='paquet de bonbons'> 
+    <container id="page-produit">
+        <img id="bonbon" src='img/bonbon1.jpg' alt='paquet de bonbons'>
+        <div class="desc">
+            <h2 id="name">Nom du produit<h2>
+            <p class="prod-desc">Type :</p>
+            <p class="prod-desc">Prix :</p>
+            <p class="prod-desc">Description :</p>
+        </div>
     </container>
 </body>

04c07f44107731dc5202575325c841a5110fdb84
Pompomtomm <tomlebreton0@gmail.com>
2024-06-12 09:10:12 +0200

    Caroussel

--- index.php
+++ index.php
@@ -2,3 +2,23 @@
 include_once('head.php');
 include_once('header.php');
 ?>
+<div class="container">
+    <div class="caroussel">
+        <div class="caroussel-inner">
+            <div class="slide">
+                <img src="img/bonbon1.jpg" alt="bonbon1">
+            </div>
+            <div class="slide">
+                <img src="img/bonbon2.jpg" alt="bonbon2">
+            </div>
+            <div class="slide">
+                <img src="img/bonbon3.jpg" alt="bonbon1">
+            </div>
+        </div>
+        <div class="controles">
+            <button id="avant">Prev</button>
+            <button id="apres">Next</button>
+        </div>
+        <div class="point-nav"></div>
+    </div>
+</div>

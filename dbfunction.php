fa3cff72b808972e1f8d4e60a76e4a237fba9a50
hugi <hugo.cossec@gmail.com>
2024-06-13 09:49:57 +0200

    Formulaire sécuriser

--- dbfunction.php
+++ dbfunction.php
@@ -1,23 +1,27 @@
 <?php
 
+//try to connect with the server using the database connection information in defined in constants.php
 try {
     $PDO = new PDO(
         DB_DRIVER . ':host=' . DB_HOST . ';dbname=' . DB_DATABASE . ';port=' . DB_PORT,
         DB_USERNAME,
         DB_PASSWORD
     );
-    $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $PDO->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (Exception $ex) {
     echo ($ex->getMessage());
     die;
 }
 
+//Magie noir mais ça marche
 function requete_preparee($sql, $params) {
     global $PDO;
+    // Requête préparer qui protège contre les injections SQL
     $stmt = $PDO->prepare($sql);
+    // Execute la requête
     $stmt->execute($params);
     return $stmt->fetchAll();
 }
 
 ?>
+
+
